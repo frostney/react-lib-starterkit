20e4ddc3a825a4ef88a8f2a9896f6b6ce81a1c6c
Johannes Stein <johannesstein@freeze-dev.com>
2016-01-29 15:25:13 +0100

    Add semicolons

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -3,11 +3,11 @@ import React, { Component, PropTypes } from 'react';
 class HelloWorld extends Component {
   static propTypes = {
     name: PropTypes.string,
-  }
+  };
 
   static defaultProps = {
     name: 'world',
-  }
+  };
 
   render() {
     return <div>Hello {this.props.name}</div>;
